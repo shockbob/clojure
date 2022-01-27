8379db998928594aa8e1b5418a241fbfed917932
shockbob <shockbob@gmail.com>
2022-01-27 15:46:49 -0700

    Delete elixir directory

--- elixir/spiral-matrix/spiral.exs
+++
@@ -1,8 +0,0 @@
-defmodule Spiral do
-  @doc """
-  Given the dimension, return a square matrix of numbers in clockwise spiral order.
-  """
-  @spec matrix(dimension :: integer) :: list(list(integer))
-  def matrix(dimension) do
-  end
-end
