dd2972d7d85ef05ef65443d443fee954c0e201ae
shockbob <shockbob@gmail.com>
2022-01-27 15:48:00 -0700

    Delete clojure/accumulate directory

--- clojure/accumulate/src/accumulate.clj
+++
@@ -1,9 +0,0 @@
-(ns accumulate)
-
-(defn accumulate 
-    ([func coll]  (accumulate func coll []))
-    ([func [f & r] result]
-        (if (nil? f)
-            result
-            (let [result (conj result (func f))]
-                (recur func r result)))))
